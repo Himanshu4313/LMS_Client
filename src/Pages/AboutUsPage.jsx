ac1cd2241b2c68eb156c164a21527483cc122dad
Himanshu4313 <hk225064@gmail.com>
2023-12-30 00:27:09 +0530

    path resolve

--- src/Pages/AboutUsPage.jsx
+++ src/Pages/AboutUsPage.jsx
@@ -1,4 +1,4 @@
-import AboutImage from "../assets/aboutmainimage.png";
+import AboutImage from "../assets/aboutmain.png";
 import CarouselSlider from "../Components/CarouselSlider";
 import { celebraties } from "../constant/SliderData";
 import HomeLayout from "../Layouts/HomeLayout";
