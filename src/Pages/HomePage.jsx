2408ac2ddb561325bbc3c7e7e8c8481362a38853
Himanshu4313 <hk225064@gmail.com>
2023-12-30 00:55:41 +0530

    image path resolve2

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-import HomePageImage from '../assets/homepageimage.png';
+import HomePageImage from '../../src/assets/homepageimage.png';
 import HomeLayout from "../Layouts/HomeLayout";
 function HomePage() {
     return (
