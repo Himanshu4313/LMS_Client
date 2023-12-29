e09fd0e1ea1480ac819a7fe90d9d68205388b4bb
Himanshu4313 <hk225064@gmail.com>
2023-12-29 22:23:58 +0530

    update live backend url

--- src/Helpers/axiosInstances.js
+++ src/Helpers/axiosInstances.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/v1";
+const BASE_URL = "https://lms-server-ndv6.onrender.com/api/v1";
 
 const axiosInstance = axios.create();
 
